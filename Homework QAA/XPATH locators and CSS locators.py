f76e5accc73918ee0481715faf1248c92042c9a6
roman <prusroman2@gmail.com>
2022-11-10 15:32:51 +0200

    homework

---
+++ Homework QAA/XPATH locators and CSS locators.py
@@ -0,0 +1,14 @@
+Site = "https://www.demoblaze.com"
+
+XPATH:
+1. header_bar = "//div[@id='navbarExample']"
+2. header_bar_home = "//li[@class='nav-item active']"
+3. header_bar_contact = "//a[@data-target='#exampleModal']"
+4. header_bar_about = "//a[@data-target='#videoModal']"
+5. header_bar_cart = "//a[@id='cartur']"
+6. header_bar_login = "//a[@data-target='#videoModal']"
+7.
+
+    main_menu_item_nokia = "//div[@id='tbodyid']/div[2]"
+ CSS:
+ 1.....
