529b1cd1c7acef80f390d487e9bf0372d1bbbff4
roman <prusroman2@gmail.com>
2022-12-03 12:11:53 +0200

    homework 13

--- Homework_QAA/home_work/tests/test_course_page.py
+++ Homework_QAA/home_work/tests/test_course_page.py
@@ -1,5 +1,6 @@
 import pytest
 
+
 @pytest.mark.smoke
 def test_play_video(open_course_page):
     course = open_course_page.play_video()
@@ -11,15 +12,18 @@ def test_hide_curriculum_about_course(scroll_course_page):
     course = scroll_course_page.hide_additional_info()
     assert course.is_info_list_displayed() is False
 
+
 @pytest.mark.regression
 def test_open_curriculum_about_course(scroll_course_page):
     course = scroll_course_page.hide_additional_info().open_info_section()
     assert course.is_info_list_second_lesson_displayed() is True
 
+
 @pytest.mark.regression
 def test_mentor_info(scroll_course_page_to_end):
     info = scroll_course_page_to_end
     assert info.is_email_is_present() is True
+
 
 @pytest.mark.smoke
 def test_start_course(scroll_course_page_to_middle):
