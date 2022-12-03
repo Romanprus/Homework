1c7909c39d36facb6249a317742fd92b6cfa9032
roman <prusroman2@gmail.com>
2022-12-03 12:45:32 +0200

    homework 14JSON

--- Homework_QAA/home_work/tests/test_all_courses.py
+++ Homework_QAA/home_work/tests/test_all_courses.py
@@ -1,15 +1,18 @@
 import pytest
 
+
 @pytest.mark.smoke
-def test_user_search(open_login_window,env): #possible captcha
+def test_user_search(open_login_window, env):  # possible captcha
     login = open_login_window.login(env.email, env.password)
     screen = login.enter_something_in_search('Das')
     assert screen.is_search_titile_visible() is True
 
+
 @pytest.mark.smoke
-def test_select_course(main_screen): #can be tested
+def test_select_course(main_screen):  # can be tested
     screen = main_screen.choose_course()
     assert screen.is_title_visible() is True
+
 
 @pytest.mark.regression
 def test_click_on_second_page(scroll_main_screen):
@@ -17,11 +20,13 @@ def test_click_on_second_page(scroll_main_screen):
     screen = scroll_main_screen.choose_page()
     assert screen.is_first_course_invisible() is False
 
+
 @pytest.mark.regression
 def test_change_page(scroll_main_screen):
     screen = scroll_main_screen.open_collections()
     screen = scroll_main_screen.next_page()
     assert screen.is_first_course_invisible() is False
+
 
 @pytest.mark.regression
 def test_open_more_courses(scroll_main_screen):
