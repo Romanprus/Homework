b63d256590c24d36c05c85b185bf2aff770c8128
roman <prusroman2@gmail.com>
2022-12-20 17:29:33 +0200

    homework 18 Pony

--- Homework_QAA/DB/repositories/product_repository.py
+++ Homework_QAA/DB/repositories/product_repository.py
@@ -1,6 +1,7 @@
 from Homework_QAA.DB.models.models import Products
 from pony.orm import db_session, left_join
 
+
 class ProductRepository():
     def __init__(self):
         self.__model = Products
@@ -11,19 +12,19 @@ class ProductRepository():
 
     @db_session
     def left_join(self):
-         results = left_join((products, orders) for products in self.__model for orders in products.orders)
-         for result in results:
-             products = result[0]
-             orders = result[1]
-             print(f"id: {products.id}, name: {products.name}, price:{products.price} quantity: {orders.quantity}, "
-                   f"total: {products.price * orders.quantity}")
+        results = left_join((products, orders) for products in self.__model for orders in products.orders)
+        for result in results:
+            products = result[0]
+            orders = result[1]
+            print(f"id: {products.id}, name: {products.name}, price:{products.price} quantity: {orders.quantity}, "
+                  f"total: {products.price * orders.quantity}")
 
 
- if __name__ == '__main__':
-     product_repo = ProductRepository()
-     product_repo.add_product('PC', 2000)
-     product_repo.add_product('X-box', 1500)
-     product_repo.add_product('PlayStation', 1000)
-     product_repo.add_product('Nokia', 5000)
-     product_repo.add_product('FunnyGun', 1000)
-     product_repo.left_join()
+if __name__ == '__main__':
+    product_repo = ProductRepository()
+    product_repo.add_product('PC', 2000)
+    product_repo.add_product('X-box', 1500)
+    product_repo.add_product('PlayStation', 1000)
+    product_repo.add_product('Nokia', 5000)
+    product_repo.add_product('FunnyGun', 1000)
+    product_repo.left_join()
